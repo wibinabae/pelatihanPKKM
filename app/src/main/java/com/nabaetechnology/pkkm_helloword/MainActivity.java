3b856d8adc5236aa32b933f84bd04be7efba819b
Syarif Hidayat <82832430+Wibinabae@users.noreply.github.com>
2022-11-21 11:29:22 +0700

    Initial commit

---
+++ app/src/main/java/com/nabaetechnology/pkkm_helloword/MainActivity.java
@@ -0,0 +1,28 @@
+package com.nabaetechnology.pkkm_helloword;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//        EditText eNIM = findViewById(R.id.edtNIM);
+//        EditText ePass = findViewById(R.id.edtPassword);
+//        Button btLogin = findViewById(R.id.btnLogin);
+//
+//        btLogin.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//
+//            }
+//        });
+    }
+}
